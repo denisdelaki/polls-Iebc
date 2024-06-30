c9a7a06f36bbe3c625c533552760d90c598c951b
denis.kipyegon@equitybank.co.ke <denis.kipyegon@equitybank.co.ke>
2024-06-30 12:29:35 +0300

    created the candidates components to handle the candidates rendering

---
+++ src/app/components/candidates/candidates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidates',
+  standalone: true,
+  imports: [],
+  templateUrl: './candidates.component.html',
+  styleUrl: './candidates.component.css'
+})
+export class CandidatesComponent {
+
+}
