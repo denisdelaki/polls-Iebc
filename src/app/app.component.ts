116294ba0619ee470607cc9413dc92e429ee6874
denis.kipyegon@equitybank.co.ke <denis.kipyegon@equitybank.co.ke>
2024-07-04 14:13:04 +0300

    modified with httpmodule to handle the routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MaterialModule } from './material/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HomeComponent, MaterialModule],
+  providers: [
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
