ed2324a89ccedecbc2b42e187fb966981b972d9f
denis.kipyegon@equitybank.co.ke <denis.kipyegon@equitybank.co.ke>
2024-07-04 14:14:33 +0300

    added the results page component to render the candidates results

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, signal, computed, effect } from '@angular/core';
+import { Candidates } from '../../interfaces/candidates';
+import { CommonModule } from '@angular/common';
+import { ServicesService } from '../../services/services.service';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './results.component.html',
-  styleUrl: './results.component.css'
+  styleUrls: ['./results.component.css']
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnInit {
+  candidatesData= signal<Candidates[]>([])
+  constructor(private service: ServicesService) {
+    effect(()=>{
+      //get candidate data
+      this.service.getCandidates().subscribe(data => {
+      this.candidatesData.set(data);
+    });
+    this.candidatesData.update
+  }, { allowSignalWrites: true })
+}
+  ngOnInit(): void {}
 
+ sortedCandidates = computed(() => {
+  return this.candidatesData().slice().sort((a: Candidates, b: Candidates) => b.votes - a.votes);
+});
 }
